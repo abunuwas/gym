09407936f7fdfe99a22154a4240f8497f43372fd
abunuwas <joseharoperalta@gmail.com>
2017-09-22 13:15:22 +0100

    Start adding functions to build calendar

--- static/js/calendar.js
+++ static/js/calendar.js
@@ -9,8 +9,35 @@ var weekdays = [
 ]
 
 function makeCalendar() {
-  var calendar = getElementById('calendar');
+  var calendar = document.getElementById('calendar');
   var today = new Date();
   var days_in_month = new Date(today.getFullYear(), today.getMonth() + 1, 0).getDate();
+  var first_weekday_month = new Date(today.getFullYear(), today.getMont(), 1).getDay();
+}
+
+
+function drawCalendar() {
+  var table = document.createElement("table");
+  table.setAttribute("id", "test");
+  table.style.backgroundColor = "black";
+  var calendar_holder = document.getElementById("calendar");
+  calendar_holder.appendChild(table);
+  var newRow = table.insertRow();
+  for (var i=0; i<7; i++) {
+    var newCell = newRow.insertCell();
+    newCell.style.backgroundColor = 'green';
+    var newDiv = document.createElement("div");
+    var newText = document.createTextNode(i);
+    newDiv.appendChild(newText);
+    newCell.appendChild(newDiv);
+    newCell.addEventListener("click", function() { alert("Hello World!")})
+  }
+}
+
+
+function sendDayData() {
 
 }
+
+drawCalendar();
+
